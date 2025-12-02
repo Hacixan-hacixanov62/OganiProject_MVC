0531e9c6768c199d28f62d4bc56b8e5c2df33d2d
Hacixan Hacixanov <hajikhanih@code.edu.az>
2025-12-02 11:46:25 +0400

    I Added Consume project part of MVC

---
+++ OganiProject_MVC/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OganiProject_MVC.Models;
+
+namespace OganiProject_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
